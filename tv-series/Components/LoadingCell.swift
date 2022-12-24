7bc6fb32e53eb9a1841246b6ac0bbcdf27b230bd
felipewil <felipewill2@gmail.com>
2022-12-24 10:25:20 -0300

    LoadingCell improvements

--- tv-series/Components/LoadingCell.swift
+++ tv-series/Components/LoadingCell.swift
@@ -11,6 +11,8 @@ class LoadingCell: UITableViewCell {
     
     private struct Consts {
         static let padding: CGFloat = 32.0
+        static let contentPadding: CGFloat = 4.0
+        static let descriptionFontSize: CGFloat = 14.0
     }
     
     // MARK: Properties
@@ -24,6 +26,23 @@ class LoadingCell: UITableViewCell {
 
         view.translatesAutoresizingMaskIntoConstraints = false
 
+        return view
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+
+        label.font = .systemFont(ofSize: Consts.descriptionFontSize, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+    
+    private lazy var wrapperView: UIView = {
+        let view = UIView()
+
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
         return view
     }()
     
@@ -41,8 +60,12 @@ class LoadingCell: UITableViewCell {
     
     // MARK: Public method
     
-    static func dequeueReusableCell(from tableView: UITableView, for indexPath: IndexPath) -> LoadingCell {
-        return tableView.dequeueReusableCell(withIdentifier: self.reuseIdentifier, for: indexPath) as! LoadingCell
+    static func dequeueReusableCell(from tableView: UITableView, description: String? = nil, for indexPath: IndexPath) -> LoadingCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.reuseIdentifier, for: indexPath) as! LoadingCell
+
+        cell.descriptionLabel.text = description
+
+        return cell
     }
     
     override func prepareForReuse() {
@@ -53,12 +76,30 @@ class LoadingCell: UITableViewCell {
     // MARK: Helpers
     
     private func setup() {
-        self.contentView.addSubview(self.activityIndicator)
+        self.contentView.addSubview(self.wrapperView)
+        self.wrapperView.addSubview(self.activityIndicator)
+        self.wrapperView.addSubview(self.descriptionLabel)
+        
+        self.descriptionLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+
+        NSLayoutConstraint.activate([
+            self.wrapperView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+            self.wrapperView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            self.wrapperView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: Consts.padding),
+            self.wrapperView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -Consts.padding),
+        ])
+
+        NSLayoutConstraint.activate([
+            self.activityIndicator.topAnchor.constraint(equalTo: self.wrapperView.topAnchor),
+            self.activityIndicator.leftAnchor.constraint(equalTo: self.wrapperView.leftAnchor),
+            self.activityIndicator.rightAnchor.constraint(equalTo: self.wrapperView.rightAnchor),
+        ])
         
         NSLayoutConstraint.activate([
-            self.activityIndicator.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
-            self.activityIndicator.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: Consts.padding),
-            self.activityIndicator.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -Consts.padding),
+            self.descriptionLabel.topAnchor.constraint(equalTo: self.activityIndicator.bottomAnchor, constant: Consts.contentPadding),
+            self.descriptionLabel.leftAnchor.constraint(equalTo: self.wrapperView.leftAnchor),
+            self.descriptionLabel.rightAnchor.constraint(equalTo: self.wrapperView.rightAnchor),
+            self.descriptionLabel.bottomAnchor.constraint(equalTo: self.wrapperView.bottomAnchor),
         ])
         
         self.activityIndicator.startAnimating()
